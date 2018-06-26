c63e3bd10dc34abaa6c5d6f7f5d4334474abe845
Jordan Harris <jharris@wework.com>
2018-06-26 17:22:48 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -3,18 +3,6 @@ def hello_t(arr)
   end
 end
 
-# call your method here!
-# it "calls the block once for each element in the passed-in array" do
-#     expect{ hello_t(names){ |name| puts name } }
-#       .to output("Tim\nTom\nJim\n")
-#       .to_stdout
-#   end
-
-#   it "returns the original array" do
-#     expect( hello_t(names){ |name| puts name } )
-#       .to eq(names)
-#   end
-
 #   it "is not hard-coded" do
 #     other_names = ["Ali", "Jasmine", "Persephone"]
 
