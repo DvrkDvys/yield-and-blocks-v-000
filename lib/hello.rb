3c7d7739af24c606f67f99ee35d6064b555fc5de
Jordan Harris <jharris@wework.com>
2018-06-26 18:23:57 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,7 @@
 def hello_t(arr)
   if block_given? == false
     puts "Hey! No block was given!\n"
-  elsif 
-  else arr.each do |name| puts name
-    end
+  else arr.each do |name| 
+    puts name
   end
 end
-
