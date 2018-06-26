9c57f06268a6c810744a0ea756fd5a525d6820c9
Jordan Harris <jharris@wework.com>
2018-06-26 18:12:06 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -6,11 +6,3 @@ def hello_t(arr)
   end
 end
 
-#   it "is not hard-coded" do
-#     other_names = ["Ali", "Jasmine", "Persephone"]
-
-#     expect{ hello_t(other_names){ |name| puts name.upcase } }
-#       .to output("ALI\nJASMINE\nPERSEPHONE\n")
-#      
-# .to_stdout
-#   end
